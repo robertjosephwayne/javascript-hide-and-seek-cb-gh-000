8d12b38bf063d88236f701448b25167df9d84809
Robert Wayne <robertjwayne@gmail.com>
2017-06-03 08:06:30 -0400

    Create function deepestChild

--- index.js
+++ index.js
@@ -8,7 +8,18 @@ function nestedTarget() {
 
 function increaseRankBy(n) {
   let ranks = document.querySelectorAll('.ranked-list li')
+
   ranks.forEach((rank) => {
     rank.innerHTML = parseInt(rank.innerHTML) + n
   })
+}
+
+function deepestChild() {
+  let deepestChild = document.querySelector('div#grand-node')
+
+  while (deepestChild.childElementCount > 0) {
+    deepestChild = deepestChild.children[0]
+  }
+
+  return deepestChild
 }
